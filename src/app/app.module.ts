8390fa2eae4ff6d3d2e52f9f47a11cc03c0ea335
arif-sk <arif.hossain177@gmail.com>
2019-12-24 16:21:48 +0600

    Project initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileInfoComponent } from './features/profile-info/profile-info.component';
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,5 @@
+import {Routes} from '@angular/router';
+import { ProfileInfoComponent } from './features/profile-info/profile-info.component';
+export const appRoutes: Routes = [
+    { path: 'profile', component: ProfileInfoComponent }
+]
